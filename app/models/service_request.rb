4bca975b882e254676f92e0ad78b99aaad3c4f77
Scott Brown <cheesetoochalk@yahoo.com>
2022-05-20 15:54:49 +0000

    Upload New File

---
+++ app/models/service_request.rb
@@ -0,0 +1,24 @@
+class ServiceRequest < ApplicationRecord
+  include Locationable
+  include Categoriable
+
+  scope :pending, -> { where status: :pending }
+  scope :in_repair, -> { where status: :in_repair }
+  scope :done, -> { where status: :done }
+
+  enum category: Shop::CATEGORIES
+  enum status: %i[pending in_repair done]
+
+  belongs_to :vehicle
+  has_many :pictures, as: :imageable, dependent: :destroy
+  has_many :offers, dependent: :destroy
+
+  delegate :client, to: :vehicle, allow_nil: true
+
+  accepts_nested_attributes_for :pictures, allow_destroy: true, reject_if: :blank?
+
+  validates :summary, :location, :category, :title, :status, presence: true
+  validates :evacuation, :repair_parts, inclusion: { in: [ true, false ] }
+  validates :estimated_budget, numericality: { greater_than_or_equal_to: 0 },
+                               if: -> { estimated_budget.present? }
+end
