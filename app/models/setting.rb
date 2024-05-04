f59e1e94df7e61aaf64143be53bfc8d59f4a034c
arnoldnekemiah <arnoldnek@gmail.com>
2024-05-04 16:36:37 +0300

    setting.rb file tweek

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,6 +1,3 @@
-# class Setting < RailsSettings::Base
-#   source Rails.root.join( 'config/app.yml' )
-# end
-class Settings < RailsSettings::CachedSettings
-  defaults[:my_awesome_settings] = 'This is my settings'
+class Setting < RailsSettings::Base
+  source Rails.root.join( 'config/app.yml' )
 end
