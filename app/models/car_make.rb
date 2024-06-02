414bdaf1043e6950301418c732f0411672d04084
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-06-02 14:01:59 +0300

    fix car make error

--- app/models/car_make.rb
+++ app/models/car_make.rb
@@ -1,8 +1,8 @@
 class CarMake < ApplicationRecord
   scope :categorized, lambda {
-    select( 'car_makes.*, car_categories.name AS category_name' )
-      .joins( :car_category )
-      .group_by( &:category_name )
+    select('car_makes.*, car_categories.name AS category_name')
+      .joins(:car_category)
+      .group_by(&:category_name)
   }
 
   has_many :car_models, dependent: :destroy
@@ -10,6 +10,10 @@ class CarMake < ApplicationRecord
 
   validates :car_category_id, presence: true
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :car_category_id }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id name created_at updated_at] # Adjust this list according to your needs
+  end
 
   def self.ransackable_associations(auth_object = nil)
     %w[car_category car_models]
