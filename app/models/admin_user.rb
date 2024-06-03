65c6f46268fea3f3f2309e4c646b6a2dc42c68be
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-06-03 18:44:48 +0300

    Racksack fix 2

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,7 +3,7 @@ class AdminUser < ApplicationRecord
 
   # Define ransackable attributes for Ransack search
   def self.ransackable_attributes(auth_object = nil)
-    %w[created_at email id remember_created_at reset_password_sent_at updated_at]
+    ["created_at", "email", "id", "remember_created_at", "reset_password_sent_at", "updated_at"]
   end
 
   # Define ransackable associations if any
