b143e373ff19d172086cd3df29b8ab8eced78093
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-06-03 18:37:44 +0300

    seach fix

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,8 @@
 class AdminUser < ApplicationRecord
   devise :database_authenticatable, :rememberable, :validatable
+
+  # Define ransackable attributes for Ransack search
+  def self.ransackable_attributes(auth_object = nil)
+    %w[created_at email id remember_created_at reset_password_sent_at updated_at]
+  end
 end
