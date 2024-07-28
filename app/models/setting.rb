0e0f493dbece56da05da1722aab638770be6262a
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-07-28 11:55:12 +0300

    included method for reset duration in setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,6 +3,10 @@ class Setting < ApplicationRecord
   validates :var, presence: true, uniqueness: { scope: [:thing_type, :thing_id] }
   validates :value, presence: true, unless: -> { value.nil? }
 
+  def self.password_reset_duration
+    find_by(var: 'password_reset_duration').value.to_i
+  end
+
   def self.method_missing(method, *args)
     setting = find_by(var: method.to_s)
     setting ? setting.value : super
