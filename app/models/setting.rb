65e848710d97703e019e820e3298134660382231
arnoldnekemiah <arnoldnek@gmail.com>
2024-09-17 12:02:17 +0300

    set default radius

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,16 +3,26 @@ class Setting < ApplicationRecord
   validates :var, presence: true, uniqueness: { scope: [:thing_type, :thing_id] }
   validates :value, presence: true, unless: -> { value.nil? }
 
+  # Example of a custom method that retrieves specific setting values
   def self.password_reset_duration
     find_by(var: 'password_reset_duration').value.to_i
   end
 
+  # Modify method_missing to handle defaults
   def self.method_missing(method, *args)
     setting = find_by(var: method.to_s)
-    setting ? setting.value : super
+    return setting.value if setting.present?
+
+    # If a default value is passed as an argument, use it
+    default_value = args.first if args.present?
+    return default_value if default_value
+
+    # If no default, raise an error as before
+    super
   end
 
   def self.respond_to_missing?(method, include_private = false)
     exists?(var: method.to_s) || super
   end
 end
+
