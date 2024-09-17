4398c234c8125d1fcb9ba45a6c66df899aaf918b
arnoldnekemiah <arnoldnek@gmail.com>
2024-09-17 15:09:53 +0300

    added card token

--- app/models/client.rb
+++ app/models/client.rb
@@ -12,12 +12,13 @@ class Client < ApplicationRecord
   delegate :email, to: :account, allow_nil: true
 
   validates :phone, uniqueness: { case_sensitive: false }, if: -> { phone.present? }
+  validates :card_token, presence: true, uniqueness: true, allow_nil: true
 
   def avatar_url
     avatar.present? ? avatar.url : ""
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    %w[address avatar created_at id id_value location name phone updated_at]
+    %w[address avatar created_at id id_value location name phone updated_at card_token]
   end
 end
