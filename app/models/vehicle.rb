2e21e877c804aa67f31a15a2be1b7d12339e7812
arnoldnekemiah <arnoldnek@gmail.com>
2024-11-17 12:58:23 +0300

    allow unasspociated cars

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,10 +2,13 @@ class Vehicle < ApplicationRecord
   mount_uploader :photo, ImageUploader
 
   has_many :service_requests, dependent: :destroy
-  belongs_to :client
+  belongs_to :client, optional: true
 
   validates :make, :model, :year, :category, presence: true
   validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # Skip validation for the client association in test and development environments
+  validates :client, presence: true, unless: -> { Rails.env.development? || Rails.env.production? }
 
   def self.ransackable_associations(auth_object = nil)
     ["client", "service_requests"]
