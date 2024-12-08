f04848bbe819032b182142b6c6569e7866bfceac
arnoldnekemiah <arnoldnek@gmail.com>
2024-12-08 12:03:00 +0300

    approve clients by default

--- app/models/account.rb
+++ app/models/account.rb
@@ -43,14 +43,21 @@ class Account < ApplicationRecord
   private
 
   def approved?
+    Rails.logger.debug("Evaluating approved? for Account ##{id}, accountable_type: #{accountable_type}")
+    
     # Ensure all clients are approved
-    return true if client?
-
+    return true if client?.tap { Rails.logger.debug("Client approved.") }
+  
     # Ensure business owners are approved only if their shop is approved
-    return accountable&.approved? if business_owner?
-
+    if business_owner?
+      Rails.logger.debug("Business owner, checking accountable approval: #{accountable&.approved?}")
+      return accountable&.approved?
+    end
+  
+    Rails.logger.debug("Approval check failed.")
     false
   end
+  
   
 
   def ensure_approved
